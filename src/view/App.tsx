f59ef8f0b47ac1ba58ff610580bdac9981c76d5f
Yariv Gilad <yariv.gilad@gmail.com>
2025-07-28 12:26:37 +0300

    updated packages versions

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -2,10 +2,15 @@
 import Card from './Card'
 import TopBar from './TopBar'
 
+
 const App = () => (
 	<div className="app">
-		<TopBar />
+		<TopBar>
+				<h1>Hello React</h1>
+		</TopBar> 
+
 		<h1 className="headline white-text">I am the main Application</h1>
+		
 		<Card action="meet" subject="friends" />
 	</div>
 )
@@ -15,9 +20,9 @@ export default App
 
 /* 
 <TopBar>
-	<div>
-		<h1>Hello React</h1>
-		<h2>Hello React</h2>
-	</div>
-</TopBar> 
+			<div>
+				<h1>Hello React</h1>
+				<h2>Hello React</h2>
+			</div>
+		</TopBar> 
 */
--- src/view/Card.tsx
+++ src/view/Card.tsx
@@ -1,61 +1,67 @@
 //   Class Components
 //-------------------------------------
-/* import { Component } from 'react'
+// import { Component } from 'react'
 
-interface Props {
-    action: string;
-    subject: string;
-}
+// import React from "react";
 
-class Card extends Component<Props> {
+// interface Props {
+//     action: string;
+//     subject: string;
+// }
 
-    myName = 'Rumpelstiltskin';
+// class Card extends Component<Props> {
 
-    constructor(props:Props) {
-		super(props);
-		console.log('in constructor()')
-		console.log(props)
-		this.myName = 'Nevuchadnetzer';
-	}
+//     myName = 'Rumpelstiltskin';
 
-	render() {
-		console.log('in render()')
-		console.log(this.props)
-		return (
-			<div className="card">
-				<h1 className="headline">I am a Card {this.myName}</h1>
-				<h2 className="paragraph">
-					{2 + 2} my goal is to {this.props.action} some {this.props.subject}
-				</h2>
-			</div>
-		)
-	}
-}
+//     constructor(props:Props) {
+// 		super(props);
+// 		console.log('in constructor()')
+// 		console.log(props)
+// 		this.myName = 'Nevuchadnetzer';
+// 	}
 
-export default Card; */
-/* //   Destructure - extract values from the props object
+// 	render() {
+// 		console.log('in render()')
+// 		console.log(this.props)
+// 		return (
+// 			<div className="card">
+// 				<h1 className="headline">I am a Card {this.myName}</h1>
+// 				<h2 className="paragraph">
+// 					{2 + 2} my goal is to {this.props.action} some {this.props.subject}
+// 				</h2>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// export default Card; 
+ //   Destructure - extract values from the props object
 //--------------------------------------------------------
-import { Component } from 'react'
+// import { Component } from 'react'
 
-interface Props {
-    action: string;
-    subject: string;
-}
-export default class Card extends Component<Props> {
-	render() {
-		const { action: act = 'something', subject } = this.props
+// interface Props {
+//     action?: string;
+//     subject: string;
+// }
+// export default class Card extends Component<Props> {
+// 	render() {
+// 		const { action:act = 'something', subject } = this.props;
+// 		// const action = this.props.action;
+// 		// const subject = this.props.subject;
 
-		return (
-			<div className="card">
-				<h1 className="headline">I am Card</h1>
-				<h2 className="paragraph">
-					i {act} {subject}
-				</h2>
-			</div>
-		)
-	}
-}
-export default Card */
+// 		// const { action: act = 'something', subject } = this.props
+
+// 		return (
+// 			<div className="card">
+// 				<h1 className="headline">I am Card</h1>
+// 				<h2 className="paragraph">
+// 					i {act} {subject}
+// 				</h2>
+// 			</div>
+// 		)
+// 	}
+// }
+// export default Card 
 
 //-------------------------------------
 //   Function Components
@@ -64,9 +70,9 @@ export default Card */
 interface Props {
 	action: string;
 	subject: string;
-  }
-  
-  export function Card(props: Props) {
+}
+
+function Card(props: Props) {
 	const { action, subject } = props;
   
 	return (
@@ -77,105 +83,107 @@ interface Props {
 		</h2>
 	  </div>
 	);
-  }
-  export default Card;
+}
+export default Card;  
   
-  /*//   Inline destructure
+//   Inline destructure
   //--------------------------
-   interface Props {
-	action: string;
-	subject: string;
-  }
+// type Props ={
+// 	action: string;
+// 	subject: string;
+// }
+
+// function Card({ action, subject }: Props) {
+// 	return (
+// 		<div className="card">
+// 			<h1 className="headline">I am a Card</h1>
+// 			<h2 className="paragraph">
+// 				i {action} {subject}
+// 			</h2>
+// 		</div>
+// 	);
+// }
+// export default Card;  
   
-  function Card({ action, subject }: Props) {
-	return (
-	  <div className="card">
-		<h1 className="headline">I am a Card</h1>
-		<h2 className="paragraph">
-		  i {action} {subject}
-		</h2>
-	  </div>
-	);
-  }
-  export default Card; */
+ //   Minimal function component
+//   //---------------------------------
+//   interface Props {
+// 	action: string;
+// 	subject: string;
+//   }
   
-  /* //   Minimal function component
-  //---------------------------------
-  interface Props {
-	action: string;
-	subject: string;
-  }
+//   const Card = ({ action, subject }: Props) => (
+// 	<div className="card">
+// 	  <h1 className="headline">I am a Card</h1>
+// 	  <h2 className="paragraph">
+// 		i {action} {subject}
+// 	  </h2>
+// 	</div>
+//   );
   
-  const Card = ({ action, subject }: Props) => (
-	<div className="card">
-	  <h1 className="headline">I am a Card</h1>
-	  <h2 className="paragraph">
-		i {action} {subject}
-	  </h2>
-	</div>
-  );
+//   export default Card; 
   
-  export default Card; */
-  
-  /* //   JSX possible outputs
+  //   JSX possible outputs
   //   container tag / React.fragment
   //   / string / null / Array of JSX blocks
   //-----------------------------------------
+
+//   interface Props {
+// 	action: string;
+// 	subject: string;
+//   }
+//   const Card = ({ action, subject }: Props) => {
+//     // return `you ${action} ${subject}`
+// 	return [
+// 			<div className="card">
+// 				<h1 className="headline">I am a Card</h1>
+// 				<h2 className="paragraph">
+// 				i {action} {subject}
+// 				</h2>
+// 			</div>,
+// 			<div className="card">
+// 				<h1 className="headline">I am a Card</h1>
+// 				<h2 className="paragraph">
+// 				i {action} {subject}
+// 				</h2>
+// 			</div>
+
+// 	]
+	  
+//   };
   
-  interface Props {
-	action: string;
-	subject: string;
-  }
-  const Card = ({ action, subject }: Props) => {
-  //   return `you ${action} ${subject}`
-	return [
-	  <div className="card">
-		<h1 className="headline">I am a Card</h1>
-		<h2 className="paragraph">
-		  i {action} {subject}
-		</h2>
-	  </div>,
-	  <div className="card">
-		<h1 className="headline">I am a Card</h1>
-		<h2 className="paragraph">
-		  i {action} {subject}
-		</h2>
-	  </div>,
-	];
-  };
+//   export default Card; 
   
-  export default Card; */
-  
-  /*//   Spread objects into props
+  //   Spread objects into props
   //---------------------------------
-   import Thumb from './Thumb'
+//    import Thumb from './Thumb'
   
-  interface Props {
-	action: string;
-	subject: string;
-  }
+//   interface Props {
+// 	action: string;
+// 	subject: string;
+//   }
   
-  function Card(props:Props) {
-	  // console.log(props)
-	  const { action, subject } = props
+//   function Card(props:Props) {
+// 	  // console.log(props)
+// 	  const { action, subject } = props
   
-	  const extraData = {
-		  action: 'kiss',
-		  day: 'Wednesday',
-		  feel: 'great'
-	  }
+// 	  const extraData = {
+// 		  action: 'kiss',
+// 		  day: 'Wednesday',
+// 		  feel: 'great'
+// 	  }
   
-	  return (
-		  <div className="card">
-			  <h1 className="headline">I am a Card</h1>
-			  <h2 className="paragraph">
-				  my goal is to {action} someeee {subject}
-			  </h2>
-			  <Thumb action={props.action} subject={props.subject} />
-			  <Thumb {...extraData} {...props} />
-		  </div>
-	  )
-  }
-  export default Card
-  //
-   */
+// 	  return (
+// 		  <div className="card">
+// 			  <h1 className="headline">I am a Card</h1>
+// 			  <h2 className="paragraph">
+// 				  my goal is to {action} someeee {subject}
+// 			  </h2>
+// 			 {/* <Thumb action={props.action} subject={props.subject} /> */}
+// 			<Thumb {...props} {...extraData}   />
+// 		  </div>
+// 	  )
+//   }
+//   export default Card
+//   //
+   
